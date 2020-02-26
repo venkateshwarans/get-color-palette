e3bf181a39b39aee74236ccbc8411c7bc7afa692
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2020-02-26 21:29:14 +0530

    create patch for all image in directory

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const Vibrant = require('node-vibrant')
 const fs = require('fs');
-const dir = './BTSONKineticManifestoFilm';
+const dir = './screenshots/BTSONKineticManifestoFilm';
 const junk = require('junk');
 const { forkJoin } = require('rxjs');
+const _ = require('lodash');
 
 
 fs.readdir(dir, (err, files) => {
-  console.log(files.filter(junk.not).length);
+  console.log(files.filter(junk.not))
+  createVibrantCall(files.filter(junk.not))
 });
 
-const imagePath = './BTSONKineticManifestoFilm/BTSONKineticManifestoFilm '
+function createVibrantCall(files) {
+  const reqArr  =[]
+  _.forEach(files, (file) => {
+    reqArr.push(Vibrant.from(`${dir}/${file}`).getPalette())
+  })
+  console.log(reqArr)
+  generatePalettes(reqArr);
+}
 
-const reqArr = [Vibrant.from(`${imagePath}(1).png`).getPalette(), Vibrant.from(`${imagePath}(2).png`).getPalette(), Vibrant.from(`${imagePath}(3).png`).getPalette()]
-
-forkJoin(reqArr).subscribe((response) => {
-  console.log(response)
-});
-
-
-
-// Vibrant.from(`${imagePath}`).getPalette()
-//   .then((palette) => console.log(palette))
+function generatePalettes(reqArr) {
+  forkJoin(reqArr).subscribe((response) => {
+    console.log(response)
+  });
+}
